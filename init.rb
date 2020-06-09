5e3e5efb744561b67cdf16614fbde2431b2373c8
root <root@0b6f58f09fa2>
2020-06-09 11:03:13 +0000

    mask account

--- init.rb
+++ init.rb
@@ -4,7 +4,6 @@ Redmine::Plugin.register :redmine_github_hook do
   name "Redmine Github Hook plugin"
   author "MacroZhao"
   description "This plugin allows your Redmine installation to receive Github post-receive notifications"
-  url "https://github.com/ilu123/redmine_github_hook"
   version RedmineGithubHook::VERSION
   settings :default => {'empty' => true}, :partial => 'settings/redmine_github_hook_settings'
 end
